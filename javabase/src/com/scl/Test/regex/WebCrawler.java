b2b91273b641b1586e7a8518dfd185c6ed1c7edd
scl <294748882@qq.com>
2017-04-25 00:34:30 +0800

    4.25

--- javabase/src/com/scl/Test/regex/WebCrawler.java
+++ javabase/src/com/scl/Test/regex/WebCrawler.java
@@ -3,7 +3,7 @@ package com.scl.Test.regex;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,35 +13,56 @@ import java.util.regex.Pattern;
 public class WebCrawler {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("这里输出的是本地连接里的的邮箱");
 		List<String> list = getmails();
 		for (String string : list) {
 			System.out.println(string);
 		}
-		
-		getmailsline();
-	}
-	
-
-	private static void getmailsline() throws IOException {
-	 URL url =new URL("https://www.baidu.com/s?wd=%40qq.com&rsv_spt=1&rsv_iqid=0xe670d19400003d63&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=cn&tn=baiduhome_pg&rsv_enter=1&oq=%25E9%2582%25AE%25E7%25AE%25B1&rsv_t=0fdbdPw9cf%2BYth72236kOPeoEuMF%2FpjKx5UDz47KPIlNVvwgg6e%2FYfdxRKVK0aW7%2BMI4&rsv_pq=9dc44ada00004373&rsv_sug3=17&rsv_sug1=10&rsv_sug7=100&rsv_sug2=0&inputT=2970327&rsv_sug4=2970332");
-		
+		System.out.println("--------------------------------------------------");
+		System.out.println("这里输出的是爬虫网络上的邮箱");
+		List<String> list1 = getmailsline();
+		for (String string : list1) {
+			System.out.println(string);
+		}
 	}
 
+	private static List<String> getmailsline() throws IOException {
+
+		URL url = new URL("http://tieba.baidu.com/p/2314539885");
+
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+		String regex = "[0-9a-zA-Z_]+@[0-9a-zA-Z]+\\.[a-z]{2,3}";
+
+		List<String> list = new ArrayList<String>();
+
+		Pattern p = Pattern.compile(regex);
+
+		String line = null;
+
+		while ((line = bReader.readLine()) != null) {
+			Matcher m = p.matcher(line);
+			while (m.find()) {
+				list.add(m.group());
+			}
+		}
+		return list; 
+	}
 
 	public static List<String> getmails() throws IOException {
-		//演示的的为爬取本地已有文件里的信息
-		//先读取到需要爬的文件
+		// 演示的的为爬取本地已有文件里的信息
+		// 先读取到需要爬的文件
 		BufferedReader br = new BufferedReader(new FileReader("D:\\GITHUB\\123.html"));
-		//定义已有的规则
+		// 定义的规则
 		String regex = "[0-9a-zA-Z_]+@[0-9a-zA-Z]+\\.[a-z]{2,3}";
-		
-		List<String> list =new ArrayList<String>();
-		
+        //通过这个
+		List<String> list = new ArrayList<String>();
+
 		Pattern p = Pattern.compile(regex);
-		
+
 		String line = null;
-		
-		while ((line = br.readLine())!= null) {
+
+		while ((line = br.readLine()) != null) {
 			Matcher m = p.matcher(line);
 			while (m.find()) {
 				list.add(m.group());
